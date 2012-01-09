58810275c988cc4bebf0cf5cbdab9de070707e7a
Ryo NAKAMURA <r7kamura@gmail.com>
2012-01-09 09:25:35 +0900

    Initial commit (-d mysql -T)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Museum::Application.config.secret_token = 'bb7f3842ba14f705f8f38a64e2a3534ce04550d01f489766ead994d98b7b25baf508639802f23efcd0c8d8f3f51fbac98515381e0a12f05c2520498cceb890d7'
